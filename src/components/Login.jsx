58df037d332c4a7da6dcc7ebc4512d3c5dae2fe7
WebersIO <muzammilmetar82@gmail.com>
2025-06-19 08:27:30 +0530

    completed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -104,17 +104,17 @@ const Login = () => {
 
     return (
         <div>
-            <div className="w-md p-5  fixed top-1/2 start-1/2 -translate-y-1/2 -translate-x-1/2">
+            <div className=" flex w-md p-5  fixed top-1/2 start-1/2 -translate-y-1/2 -translate-x-1/2">
 
 
 
 
 
                 <section className="bg-gray-50 my-5 ">
-                    <div className="flex flex-col items-center justify-center px-6 py-1 mx-auto md:h-screen lg:py-0 my-5 ">
+                    <div className="flex flex-col items-center justify-center px-6 py-1 mx-auto  md:flex-row md:h-screen lg:py-0 my-5">
 
-                        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <div className="w-min-[200px] bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                            <div className="p-6 space-y-4 md:space-y-6 sm:p-8 w-[300px]">
                                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                                     Create an account
                                 </h1>
